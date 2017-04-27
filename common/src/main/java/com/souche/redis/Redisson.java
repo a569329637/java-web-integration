30b85c6b428ec1358b9cdeee00539ea172b029e7
shangquan <guishangquan@souche.com>
2017-04-27 18:57:12 +0800

    增加 redisson 客户端

---
+++ common/src/main/java/com/souche/redis/Redisson.java
@@ -0,0 +1,38 @@
+package com.souche.redis;
+
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
+
+/**
+ * @author guishangquan
+ * @Description
+ * @Package com.souche.redis
+ * @date 17/4/27
+ **/
+public class Redisson {
+
+    private RedissonClient redissonClient;
+
+
+    public void init() {
+        String address = "127.0.0.1:6379";
+        String database = "0";
+        String password = "";
+
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(address)
+                .setDatabase(Integer.parseInt(database))
+                .setPassword(password);
+        config.setCodec(StringCodec.INSTANCE);
+
+        redissonClient = org.redisson.Redisson.create(config);
+    }
+
+    public void destroy() {
+        if (redissonClient != null) {
+            redissonClient.shutdown();
+        }
+    }
+}
