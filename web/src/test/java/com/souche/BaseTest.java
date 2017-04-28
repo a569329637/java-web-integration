c8190626c0d07b7f07ce63abd581c713c841294c
shangquan <guishangquan@souche.com>
2017-04-28 13:58:13 +0800

    增加对 UserDao 的测试

--- web/src/test/java/com/souche/BaseTest.java
+++ web/src/test/java/com/souche/BaseTest.java
@@ -1,12 +1,9 @@
 package com.souche;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author guishangquan
@@ -16,11 +13,5 @@ import org.springframework.transaction.annotation.Transactional;
  **/
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:spring/spring.xml") // Spring配置文件的入口
-@Transactional // 确认有 transactionManager 这个 bean
-@Rollback // 默认回滚
-public class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
-
-    @Test
-    public void test() {}
-
+public abstract class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
 }
