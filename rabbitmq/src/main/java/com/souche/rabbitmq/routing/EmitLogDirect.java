19f9c6ed18911e1e19559f79677b3ba6827a1adb
guishangquan <569329637@qq.com>
2017-11-17 18:48:42 +0800

    routing 消息路由

---
+++ rabbitmq/src/main/java/com/souche/rabbitmq/routing/EmitLogDirect.java
@@ -0,0 +1,59 @@
+package com.souche.rabbitmq.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by Administrator on 2017/11/17.
+ */
+public class EmitLogDirect {
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+        String severity = getSeverity(args);
+        String message = getMessage(args);
+
+        channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes());
+        System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+    private static String getSeverity(String[] strings) {
+        if (strings.length < 1) {
+            return "warning";
+        }
+        return strings[0];
+    }
+
+    private static String getMessage(String[] strings) {
+        if (strings.length < 2) {
+            return "Hello World!...";
+        }
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0 || length == 1) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder(strings[1]);
+        for (int i = 2; i < length; ++ i) {
+            stringBuilder.append(delimiter).append(strings[i]);
+        }
+        return stringBuilder.toString();
+    }
+}
